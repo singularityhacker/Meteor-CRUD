310b32f441d12b37ca1134a102d904a0d1aaf7c2
Justice Conder <justiceconder@gmail.com>
2020-11-22 15:11:50 -0500

    to-do app final touches

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,19 +1,17 @@
 import { Template } from 'meteor/templating';
 
 import { Tasks } from '../api/tasks.js';
- 
+
+import './task.js';
 import './body.html';
  
 Template.body.helpers({
 
-  // tasks: [
-  //   { text: 'This is task 1' },
-  //   { text: 'This is task 2' },
-  //   { text: 'This is task 3' },
-  // ],
+
 
   tasks() {
-    return Tasks.find({});
+    // Show newest tasks at the top
+    return Tasks.find({}, { sort: { createdAt: -1 } });
   },
 
 });
